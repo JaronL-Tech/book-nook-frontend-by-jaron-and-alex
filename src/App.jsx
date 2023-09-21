4321fc96628d6efbbbf064b193e4cec1891f4d6d
Jaron <Jaronleach98@gmail.com>
2023-09-21 11:21:32 -0400

    Edit code.

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import SearchPage from "./pages/SearchPage/Searchpage.Jsx";
+// import SearchPage from "./pages/SearchPage/Searchpage.Jsx";
+import BookDetailsPage from "./pages/BookDetailsPage/BookDetailsPage.Jsx";
+
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import SearchBar from "./components/Searchbar/Searchbar";
+// import SearchBar from "./components/Searchbar/Searchbar";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
 import { useState } from "react";
 import axios from "axios";
+import ReviewForm from "./components/ReviewForm/ReviewForm";
 
 function App() {
   const [Book, setBook] = useState([]);
@@ -45,7 +48,8 @@ function App() {
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/search" element={<SearchPage setBook={setBook} />} />
+        {/* <Route path="/search" element={<SearchPage setBook={setBook} />} /> */}
+        <Route path="/reviewform" element={<ReviewForm />} />
       </Routes>
 
       <Footer />
--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,5 +1,52 @@
-const ReviewForm = ({}) => {
-  return <></>;
+import axios from "axios";
+import react from "react";
+import { useState } from "react";
+
+const ReviewForm = ({ onNewBook }) => {
+  const [submitting, setsubmitting] = useState(false);
+  const [BookId, setBookId] = useState("");
+  const [Text, setText] = useState("");
+  const [Rating, setRating] = useState("");
+
+  const handdleSubmit = async (e) => {
+    e.preventDefault();
+    setsubmitting(true);
+
+    const formData = [BookId, Text, Rating];
+
+    try {
+      const response = await axios.post(
+        "https://localhost:5001/api/reviews/",
+        formData
+      );
+      if (response.status === 201) {
+        onNewBook();
+      }
+    } catch (error) {
+      console.warn("Error submitting new Title form");
+    }
+  };
+
+  return (
+    <form onSubmit={handdleSubmit} className="Flex-item">
+      <h4>Add Review</h4>
+      <div>
+        <div>
+          <label>BookId</label>
+          <input value={BookId} onChange={(e) => setBookId(e.target.value)} />
+        </div>
+        <div>
+          <label>Text</label>
+          <input value={Text} onChange={(e) => setText(e.target.value)} />
+        </div>
+        <div>
+          <label>Rating</label>
+          <input value={Rating} onChange={(e) => setRating(e.target.value)} />
+        </div>
+      </div>
+      <button type="submit">Add Review</button>
+    </form>
+  );
 };
 
 export default ReviewForm;
