4cc4c27b379dcc93c28afc123335c289a30f523c
Jaron <Jaronleach98@gmail.com>
2023-09-21 15:08:49 -0400

    worked on review and reviewlist.

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,14 @@
-const Review = ({}) => {
-  return <div></div>;
+const Review = ({ bookid }) => {
+  const handleclick = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="review">
+      <span>{bookid}</span>
+      <button onClick={handleclick}>Favorite</button>
+    </div>
+  );
 };
 
 export default Review;
--- src/components/ReviewList/ReviewList.jsx
+++ src/components/ReviewList/ReviewList.jsx
@@ -1,7 +1,16 @@
 import Review from "../Review/Review";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import { useState } from "react";
 const ReviewList = ({}) => {
+  const Books = [FetchBooks];
   const reviewItems = reviews.map((review) => <Review />);
-  return <ul></ul>;
+  return (
+    <div className="flex-item">
+      <h4>My Reviews</h4>
+      <div>{Review}</div>
+    </div>
+  );
 };
 
 export default ReviewList;
