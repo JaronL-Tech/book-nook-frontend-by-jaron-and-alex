af7766634047c9b0ac814710f3ae291a3f6fc605
Jaron <Jaronleach98@gmail.com>
2023-09-22 15:26:15 -0400

    Fixed items props error in bookdetails page and changed navbar to homepage.

--- src/components/ReviewList/ReviewList.jsx
+++ src/components/ReviewList/ReviewList.jsx
@@ -1,19 +1,22 @@
-/* import Review from "../Review/Review";
-import axios from "axios";
-import useAuth from "../../hooks/useAuth";
-import { useState } from "react";
-const ReviewList = ({}) => {
-    const Books = [
-    FetchBooks
-  ];
-  const reviewItems = reviews.map((review) => <Review />);
-  return (
-    <div className="flex-item">
-      <h4>My Reviews</h4>
-      <div>{Review}</div>
-    </div>
-  );
-};
+// import Review from "../Review/Review";
+// import axios from "axios";
+// import { useState } from "react";
+// import react from "react";
 
-export default ReviewList;
- */
+// var Function  {
+//   const ReviewList = ({ fetchBookDetails}) => {
+//   const reviewItems = ReviewList.map((review) => <Review />);
+//   <li key={ReviewList.toString()}>
+// {ReviewList}
+//   </li>
+//   return (
+//     <div className="flex-item">
+//      <h2>ReviewList</h2>
+//      <ul>{ReviewList}</ul>
+//     </div>
+//   );
+// }
+
+// };
+
+// export default ReviewList;
