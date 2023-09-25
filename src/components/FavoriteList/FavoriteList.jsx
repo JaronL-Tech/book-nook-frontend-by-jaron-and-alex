1a0403876ab361af699a23cedb133a564a1fea1c
Jaron <Jaronleach98@gmail.com>
2023-09-24 20:14:23 -0400

    Created and work on favorites list.

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,5 +1,12 @@
-const FavoriteList = ({}) => {
-  return <></>;
+import axios from "axios";
+import react from "react";
+import { useState } from "react";
+
+const FavoriteList = ({BookDetailsDto, bookId }) => {
+  const ListofFavorites = BookDetailsDto.Favorite;
+  const Favorite =ListofFavorites.map((Favorite)) => (<Favorite key={favorite.id} favorite={favorite}  />)
+  return <div>{Favorite}</div>
+
 };
 
 export default FavoriteList;
