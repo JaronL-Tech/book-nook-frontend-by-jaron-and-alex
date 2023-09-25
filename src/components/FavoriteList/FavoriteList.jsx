588cd1433926cdb5ae6f960c6a240f89e8b02385
Jaron <Jaronleach98@gmail.com>
2023-09-25 13:38:26 -0400

    Fixed errors in code and connectedd Favorites list to favoritesPages.

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -3,11 +3,11 @@ import react from "react";
 import { useState } from "react";
 import Favorite from "../Favorite/Favorite";
 
-const FavoriteList = ({favorites }) => {
-
-  const Listoffavorites = favorites.map((favorite)) => (<Favorite key={favorite.id} favorite={favorite}  />)
-  return (<div>{Listoffavorites}</div>)
-
+const FavoriteList = ({ favorites }) => {
+  const Listoffavorites = favorites.map((favorite) => (
+    <Favorite key={favorite.id} favorite={favorite} />
+  ));
+  return <div>{Listoffavorites}</div>;
 };
 
 export default FavoriteList;
